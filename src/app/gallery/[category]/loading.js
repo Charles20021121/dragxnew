21344e843c3f8a95dd432d5cdcf6e95c3eeaa8a8
Charles <htechcodesolution888@gmail.com>
2026-06-24 16:02:36 +0800

    save

---
+++ src/app/gallery/[category]/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 pt-24 pb-12">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-[#1c5434]"></div>
+    </div>
+  );
+}
